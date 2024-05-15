fc8d99e63f4ac2d327622a912e0083a74957361e
Nez <nezli2808@gmail.com>
2024-05-15 20:35:09 +0700

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+import Preloader from '../src/component/preload.jsx';
 import Tiktok from "./section/new.jsx";
 import Landing from "../src/section/lp.jsx"
 
@@ -9,11 +9,25 @@ import Landing from "../src/section/lp.jsx"
 function App() {
   const [count, setCount] = useState(0);
 
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Fungsi simulasi delay untuk menampilkan preloader dalam beberapa detik
+    const delay = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000); // Ganti nilai 3000 menjadi waktu yang diinginkan dalam milidetik
+    return () => clearTimeout(delay); // Membersihkan timeout saat komponen unmount
+  }, []);
+
   return (
     <Router>
+    {isLoading ? (
+        <Preloader /> // Tampilkan preloader jika isLoading true
+      ) : (
       <Routes>
         <Route path="/" element={<Landing />} />
       </Routes>
+    )}
     </Router>
   );
 }
--- src/component/aboutme.jsx
+++ src/component/aboutme.jsx
@@ -1,10 +1,29 @@
 import "../section/new.css"
 import vannes from "../assets/vannes2.jpg";
+import React, { useEffect, useRef } from 'react';
+import gsap from 'gsap';
 function Aboutme(){
+    const paragraphRef = useRef(null);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('is-visible'); // Tambahkan kelas is-visible saat masuk viewport
+                } else {
+                    entry.target.classList.remove('is-visible'); // Hapus kelas is-visible saat keluar viewport
+                }
+            });
+        });
+
+        observer.observe(paragraphRef.current); // Mulai mengamati elemen saat komponen dimuat
+
+        return () => observer.disconnect(); // Hentikan observasi saat komponen di-unmount
+    }, []);
     return(
         <>
             <div className="tittle">
-                    <h1>About me</h1>
+                    <h1 >About me </h1>
                 </div>
                 
                 <div className="about">
@@ -13,15 +32,17 @@ function Aboutme(){
                             <img src={vannes}></img>
                         </div>
                         <div className="about-right">
-                            <p>Hi, im Vannes</p>
-                            <div className="paragraph">
-                                <p>Someone who likes creating interfaces with a modern and futuristic appearance based on websites. I like creating designs, drawings and programs. So, I chose to pursue an opportunity to experience website design and interface programming
-                                <br/>In my opinion, seeking experience is important for a person's future career because from these experiences, we can know how to face, change and overcome a problem. Therefore, I want to introduce the world of work through the Internship Season 2 opportunity at Cretivox. I don't have a lot of experience and portfolio, but with my willingness to keep learning and being willing to try things I can't yet, I'm sure I can give my best.</p>
+                            <p>Hi, im Vannes 😎</p>
+                            <div className="paragraph" ref={paragraphRef} >
+                                <p>Someone who likes creating interfaces with a modern and futuristic appearance based on websites. I like creating designs, drawings and programs. So, I chose to pursue an opportunity to experience website design and interface programming.<br/>
+                                <br/>In my opinion, seeking experience is important for a person's future career because from these experiences, we can know how to face, change and overcome a problem. I really want to join Cretivox because I want to try a more productive 
+                                and relevant world of work. seeing the huge influence, popularity and interest, I think Cretivox is the right place for me to gain knowledge and be creative
+                                Therefore, I want to introduce the world of work through the Internship Season 2 opportunity at Cretivox. I don't have a lot of experience and portfolio, but with my willingness to keep learning and being willing to try things I can't yet, I'm sure I can give my best.</p>
                             </div>
                         </div>
                     </div>
-                    <div className="quotes">
-                        <h2>i believe</h2>
+                    <div className="quotes" >
+                        <h2>I Believe</h2>
                         <p>"ability is not only measured by his ability to do something, but also by his willingness to develop and learn"</p>
                     </div>
                 </div>
